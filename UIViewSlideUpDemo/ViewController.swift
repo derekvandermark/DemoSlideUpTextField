2bd9cf29ad19ae7d93b4e984860d73363dfd4f63
derekvandermark <derek.vandermark@gmail.com>
2020-09-14 21:33:51 -0400

    first commit

--- UIViewSlideUpDemo/ViewController.swift
+++ UIViewSlideUpDemo/ViewController.swift
@@ -10,11 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var sampleView: UIView!
+    @IBOutlet weak var sampleLabel: UILabel!
+    @IBOutlet weak var smapleTextfield: UITextField!
+    @IBOutlet weak var sampleButton: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIWindow.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIWindow.keyboardWillHideNotification, object: nil)
+        
+        let radiusViewConstraint = NSLayoutConstraint(item: sampleView!, attribute: .bottom, relatedBy: .equal, toItem: super.view, attribute: .bottom, multiplier: 1.0, constant: -30.0)
+        
+        view.addConstraint(radiusViewConstraint)
+        print(view.constraints)
+        
     }
 
-
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            smapleTextfield.endEditing(true)
+        }
+    }
+    
+    
 }
 
+extension ViewController {
+   
+   @objc func keyboardWillShow(notification: NSNotification) {
+        print("keyboardWillShow")
+       if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+           
+           self.sampleView.center.y += (-1 * keyboardSize.height)
+           view.constraints[2].constant += (-1 * keyboardSize.height)
+       
+       UIView.animate(withDuration: 0.5, animations: {
+
+            self.view.layoutIfNeeded()
+
+        })
+       }
+   }
+
+      @objc func keyboardWillHide(notification: NSNotification){
+        print("keyboardWillHide")
+       if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+
+           self.sampleView.center.y += 1 * keyboardSize.height
+           view.constraints[2].constant -= (-1 * keyboardSize.height)
+
+        UIView.animate(withDuration: 0.5, animations: {
+
+            self.view.layoutIfNeeded()
+
+        })
+       }
+   
+   }
+
+}
